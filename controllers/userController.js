3e76dea450ba993db394909b84457fd85b905dd6
anhkhoi9999 <khoinase180584@fpt.edu.vn>
2026-09-21 11:00:09 +0700

    Initial backend setup

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+const User = require('../models/userModel');
+
+// @desc    Get all users
+// @route   GET /api/users
+// @access  Public
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// @desc    Create a new user
+// @route   POST /api/users
+// @access  Public
+const createUser = async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    if (!name || !email) {
+      return res.status(400).json({ message: 'Please provide name and email' });
+    }
+    const user = await User.create({ name, email });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getUsers,
+  createUser,
+};
